ac1c9de1e1b7f735f8672227f78b597826790eec
Novia Lo <lonovia@gmail.com>
2021-01-06 19:21:42 +0800

    add guess times

--- 47guess.py
+++ 47guess.py
@@ -1,13 +1,17 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count = count + 1 # count += 1
 	num = input('請輸入數字: ')
 	num = int(num)
 	if num == r:
 		print('你猜對了!')
+		print('你猜了第', count, '次')
 		break
 	elif num > r:
 		print('你的答案比較大')
 	elif num < r:
 		print('你的答案比較小')
+	print('你猜了第', count, '次')
 
