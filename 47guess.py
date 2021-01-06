3380aa7221f2a197554e31483f29b6bcb7794d36
Novia Lo <lonovia@gmail.com>
2021-01-06 19:15:44 +0800

    first version

---
+++ 47guess.py
@@ -0,0 +1,13 @@
+import random
+r = random.randint(1, 100)
+while True:
+	num = input('請輸入數字: ')
+	num = int(num)
+	if num == r:
+		print('你猜對了!')
+		break
+	elif num > r:
+		print('你的答案比較大')
+	elif num < r:
+		print('你的答案比較小')
+
